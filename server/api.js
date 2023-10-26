cc2bf5353989268a8eebae3e402286b686fdeddd
shanthosh04 <133089176+shanthosh04@users.noreply.github.com>
2023-10-26 08:30:02 +0200

    delete

--- server/api.js
+++ server/api.js
@@ -1,8 +1,8 @@
 const sqlite3 = require('sqlite3').verbose();
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken'); // Importieren des jsonwebtoken-Pakets
+const jwt = require('jsonwebtoken'); 
 
-// Öffnen der Datenbankverbindung
+
 const db = new sqlite3.Database('./db/chinook.db', sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
     console.error(err.message);
@@ -13,7 +13,7 @@ const db = new sqlite3.Database('./db/chinook.db', sqlite3.OPEN_READWRITE, (err)
 const login = async (req, res) => {
   const { username, password } = req.body;
 
-  // Abfrage des Benutzers aus der Datenbank anhand des Benutzernamens
+
   const query = 'SELECT * FROM Benutzer WHERE Benutzername = ?';
   db.get(query, [username], (err, row) => {
     if (err) {
@@ -33,22 +33,22 @@ const login = async (req, res) => {
       }
 
       if (result) {
-        // Authentifizierung erfolgreich
+   
         const tokenData = {
           username: row.Benutzername,
         };
 
-        // Token generieren (mit Ablaufzeit von 1 Stunde)
+
         const token = jwt.sign(tokenData, 'IhrGeheimesToken', { expiresIn: '1h' });
 
-        // Token an den Client zurückgeben
+   
         return res.status(200).json({ token });
       } else {
-        // Passwörter stimmen nicht überein, 401 Unauthorized zurückgeben
+
         return res.status(401).json({ error: 'Unauthorized' });
       }
     });
   });
 };
 
-module.exports = { login }; // Hier ändert sich der Export auf die `login`-Funktion
+module.exports = { login };
